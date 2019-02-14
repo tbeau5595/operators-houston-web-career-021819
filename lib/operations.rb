9986ae562a264c797683bead5c4d478595275bf5
Trey <tbeau5595@hotmail.com>
2019-02-14 21:55:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,56 @@
 def unsafe?(speed)
-
+  if 40 > speed < 60
+    retrun true
+  else false
 end
 
 
 
 def not_safe?(speed)
-	
+  if 40 > speed < 60 ? true: false
 end
+
+
+
+
+
+
+
+
 	
+def unsafe?(speed)
+  if speed < 40 || speed > 60
+    return true
+  return false
+end
+
+
+def not_safe?(speed)
+  return speed < 40 || speed > 60 ? true: false
+end
+
+
+
+
+
+def unsafe?(speed)
+  if speed < 40 || speed > 60
+    puts TRUE
+  else
+    FALSE
+end
+
+
+
+def not_safe?(speed)
+  speed ? true : false
+end
+
+
+
+
+
+
+
 
 
